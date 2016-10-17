b83290158cd675400361b26823a72d2ffe026e4a
Sebastián Alvarez <seba.alvarez16@gmail.com>
2016-10-17 13:54:01 -0300

    Adding tests

---
+++ tests/test.js
@@ -0,0 +1,41 @@
+var sinon = require('sinon');
+var chai = require('chai');
+var expect = chai.expect;
+
+var mongoose = require('mongoose');
+require('sinon-mongoose');
+
+//Importing our todo model for our unit testing.
+var Donor = require('../app/models/donor');
+
+describe("Get all donors", function(){
+	// Test will pass if we get all todos
+	it("should return all donors", function(done){
+	    var DonorMock = sinon.mock(Donor);
+	    var expectedResult = {status: true, donor: []};
+
+	    DonorMock.expects('find').yields(null, expectedResult);
+	    Donor.find(function (err, result) {
+		Donor.verify();
+		Donor.restore();
+		expect(result.status).to.be.true;
+		done();
+	    });
+
+	});
+
+/*
+// Test will pass if we fail to get a todo
+it("should return error", function(done){
+    var TodoMock = sinon.mock(Todo);
+    var expectedResult = {status: false, error: "Something went wrong"};
+    TodoMock.expects('find').yields(expectedResult, null);
+    Todo.find(function (err, result) {
+	TodoMock.verify();
+	TodoMock.restore();
+	expect(err.status).to.not.be.true;
+	done();
+    });
+});
+*/
+});
