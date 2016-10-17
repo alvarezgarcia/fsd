9f55a7e785e5d1d58936dbb799b906fa21278eed
Sebastián Alvarez <seba.alvarez16@gmail.com>
2016-10-17 15:13:48 -0300

    Solver routing of home

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ var mongoose = require('mongoose');
 var routes = require('./app/routes.js');
 var config = {};
 
+
 config.port = 8080;
 mongoose.connect('mongodb://localhost/blood-donor');
 
 routes(app);
 
+app.use(express.static('./public/dist'));
 app.listen(config.port);
 console.log("Blood donor service started in "+config.port);
