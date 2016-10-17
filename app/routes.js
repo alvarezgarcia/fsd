2c1d9fdfb868be40435ef8d3855abf672021972b
Sebastián Alvarez <seba.alvarez16@gmail.com>
2016-10-17 12:48:24 -0300

    First implementation of endpoints

--- app/routes.js
+++ app/routes.js
@@ -1,1 +1,11 @@
 var Donor = require('./models/donor');
+
+module.exports = function(app) {
+
+	app.get('/api/donors', function(req, res) {
+		Donor.find(function(err, ds) {
+			res.json(ds);
+		});
+	});
+
+};
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var app = express();
 
-var mongoose = require('mongoose');
+var routes = require('./app/routes.js');
 
+var config = {};
+config.port = 8080;
+
+var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/blood-donor');
 
+routes(app);
 
-app.listen(8080);
+
+app.listen(config.port);
+console.log("Blood donor service started in "+config.port);
