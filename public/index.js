b24bfe9c319e7544dc5976634412285752760edc
Sebastián Alvarez <seba.alvarez16@gmail.com>
2016-10-18 00:28:11 -0300

    Working route example

--- public/index.js
+++ public/index.js
@@ -1,56 +1,88 @@
 import React from 'react'
-import { render } from 'react-dom'
+import ReactDOM from 'react-dom'
 
 // First we import some modules...
-import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router'
+//import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router'
+import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router'
 
-// Then we delete a bunch of code from App and
-// add some <Link> elements...
-const App = React.createClass({
-  render() {
+
+//var { Router, Route, IndexRoute, Link, browserHistory } = ReactRouter
+
+var MainLayout = React.createClass({
+  render: function() {
     return (
-      <div>
-        <h1>App</h1>
-        {/* change the <a>s to <Link>s */}
-        <ul>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/inbox">Inbox</Link></li>
-        </ul>
-
-        {this.props.children}
+      <div className="app">
+        <header className="primary-header"></header>
+        <aside className="primary-aside">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/users">Users</Link></li>
+            <li><Link to="/widgets">Widgets</Link></li>
+          </ul>
+        </aside>
+        <main>
+          {this.props.children}
+        </main>
       </div>
-    )
+      )
   }
 })
 
-const Home = React.createClass({
-  render() {
-    return (
-      <div>
-        <h1>Home</h1>
-      </div>
-    )
+var Home = React.createClass({
+  render: function() {
+    return (<h1>Home Page</h1>)
   }
 })
 
-const About = React.createClass({
-  render() {
+var SearchLayout = React.createClass({
+  render: function() {
     return (
-      <div>
-        <h1>About</h1>
+      <div className="search">
+        <header className="search-header"></header>
+        <div className="results">
+          {this.props.children}
+        </div>
+        <div className="search-footer pagination"></div>
       </div>
-    )
+      )
   }
 })
 
+var UserList = React.createClass({
+  render: function() {
+    return (
+      <ul className="user-list">
+        <li>Dan</li>
+        <li>Ryan</li>
+        <li>Michael</li>
+      </ul>
+      )
+  }
+})
 
-// Finally, we render a <Router> with some <Route>s.
-// It does all the fancy routing stuff for us.
-render((
-  <Router history={hashHistory}>
-    <Route path="/" component={App}>
+var WidgetList = React.createClass({
+  render: function() {
+    return (
+      <ul className="widget-list">
+        <li>Widget 1</li>
+        <li>Widget 2</li>
+        <li>Widget 3</li>
+      </ul>
+      )
+  }
+})
+
+// Note that with how CodePen works, I wasn't able to get the browserHistory to work
+// as the article suggests. The demo works without it, but you'll want to be sure to 
+// use it in a real application
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={MainLayout}>
       <IndexRoute component={Home} />
-      <Route path="about" component={About} />
+      <Route component={SearchLayout}>
+        <Route path="users" component={UserList} />
+        <Route path="widgets" component={WidgetList} />
+      </Route> 
     </Route>
   </Router>
-), document.body)
+), document.getElementById('app'))
